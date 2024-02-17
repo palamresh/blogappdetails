3945b953842a9cdff4a8035b1f237c2ba823b1b9
palamresh <palamresh602@gamil.com>
2024-02-17 11:46:27 +0530

    download con,image,

---
+++ lib/notification/download_image_video.dart
@@ -0,0 +1,178 @@
+import 'dart:io';
+import 'dart:typed_data';
+import 'dart:ui' as ui;
+
+import 'package:device_info_plus/device_info_plus.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+// ignore: depend_on_referenced_packages
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:saver_gallery/saver_gallery.dart';
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  GlobalKey _globalKey = GlobalKey();
+
+  @override
+  void initState() {
+    super.initState();
+
+    _requestPermission();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Save image to gallery"),
+        ),
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              RepaintBoundary(
+                key: _globalKey,
+                child: Center(
+                  child: Image.network(
+                      'https://tse2.mm.bing.net/th?id=OIP.E5XRsXOXAY5pFnhMKDoU8gHaEo&pid=Api&P=0&h=220',
+                      fit: BoxFit.contain),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 15),
+                child: ElevatedButton(
+                  onPressed: _saveScreen,
+                  child: Text("Save Local Image"),
+                ),
+                width: 200,
+                height: 44,
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 15),
+                child: ElevatedButton(
+                  onPressed: _getHttp,
+                  child: Text("Save network image"),
+                ),
+                width: 200,
+                height: 44,
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 15),
+                child: ElevatedButton(
+                  onPressed: _saveVideo,
+                  child: Text("Save network video"),
+                ),
+                width: 200,
+                height: 44,
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 15),
+                child: ElevatedButton(
+                  onPressed: _saveGif,
+                  child: Text("Save Gif to gallery"),
+                ),
+                width: 200,
+                height: 44,
+              ),
+            ],
+          ),
+        ));
+  }
+
+  _requestPermission() async {
+    bool statuses;
+    if (Platform.isAndroid) {
+      final deviceInfoPlugin = DeviceInfoPlugin();
+      final deviceInfo = await deviceInfoPlugin.androidInfo;
+      final sdkInt = deviceInfo.version.sdkInt;
+      statuses =
+          sdkInt < 29 ? await Permission.storage.request().isGranted : true;
+      statuses = await Permission.storage.request().isGranted;
+    } else {
+      statuses = await Permission.photosAddOnly.request().isGranted;
+    }
+    _toastInfo('requestPermission result: ${statuses}');
+  }
+
+  _saveScreen() async {
+    RenderRepaintBoundary boundary =
+        _globalKey.currentContext!.findRenderObject() as RenderRepaintBoundary;
+    ui.Image image = await boundary.toImage();
+    ByteData? byteData = await image.toByteData(format: ui.ImageByteFormat.png);
+    if (byteData != null) {
+      String picturesPath = "${DateTime.now().millisecondsSinceEpoch}.jpg";
+      final result = await SaverGallery.saveImage(byteData.buffer.asUint8List(),
+          name: picturesPath, androidExistNotSave: false);
+      debugPrint(result.toString());
+      _toastInfo(result.toString());
+    }
+  }
+
+  _getHttp() async {
+    var response = await Dio().get(
+        "https://ss0.baidu.com/94o3dSag_xI4khGko9WTAnF6hhy/image/h%3D300/sign=a62e824376d98d1069d40a31113eb807/838ba61ea8d3fd1fc9c7b6853a4e251f94ca5f46.jpg",
+        options: Options(responseType: ResponseType.bytes));
+    String picturesPath = "test_jpg.jpg";
+    debugPrint(picturesPath);
+    final result = await SaverGallery.saveImage(
+        Uint8List.fromList(response.data),
+        quality: 60,
+        name: picturesPath,
+        androidRelativePath: "Pictures/aa/bb",
+        androidExistNotSave: true);
+    debugPrint(result.toString());
+    _toastInfo("$result");
+  }
+
+  _saveGif() async {
+    var response = await Dio().get(
+        "https://hyjdoc.oss-cn-beijing.aliyuncs.com/hyj-doc-flutter-demo-run.gif",
+        options: Options(responseType: ResponseType.bytes));
+    String picturesPath = "test_gif";
+    debugPrint(picturesPath);
+    final result = await SaverGallery.saveImage(
+        Uint8List.fromList(response.data),
+        quality: 60,
+        name: picturesPath,
+        androidRelativePath: "Pictures/appName/xx",
+        androidExistNotSave: false);
+    debugPrint(result.toString());
+    _toastInfo("$result");
+  }
+
+  _saveVideo() async {
+    final dir = await getTemporaryDirectory();
+    String savePath =
+        "${dir.path}/${DateTime.now().millisecondsSinceEpoch}.mp4";
+    String fileUrl =
+        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4";
+    await Dio().download(
+      fileUrl,
+      savePath,
+      // options: Options(
+      //   sendTimeout: 10 * 60 * 1000,
+      //   receiveTimeout: 10 * 60 * 1000,
+      // ),
+      onReceiveProgress: (count, total) {
+        debugPrint((count / total * 100).toStringAsFixed(0) + "%");
+      },
+    );
+    final result = await SaverGallery.saveFile(
+        file: savePath,
+        androidExistNotSave: true,
+        name: '123.mp4',
+        androidRelativePath: "Movies");
+    debugPrint(result.toString());
+    _toastInfo("$result");
+  }
+
+  _toastInfo(String info) {
+    // Fluttertoast.showToast(msg: info, toastLength: Toast.LENGTH_LONG);
+  }
+}
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,7 @@
 import FlutterMacOS
 import Foundation
 
+import device_info_plus
 import file_selector_macos
 import firebase_auth
 import firebase_core
@@ -13,8 +14,10 @@ import firebase_messaging
 import firebase_storage
 import flutter_local_notifications
 import flutter_timezone
+import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
@@ -23,4 +26,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FlutterTimezonePlugin.register(with: registry.registrar(forPlugin: "FlutterTimezonePlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
